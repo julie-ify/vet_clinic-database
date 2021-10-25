9ac7668cb02c782718136b4993cb50a22a06d7ea
Juliana Ifionu <julieifionu@gmail.com>
2021-10-25 15:13:44 +0100

    Create animals table for vet_clinic database

--- schema.sql
+++ schema.sql
@@ -2,4 +2,15 @@
 
 CREATE DATABASE vet_clinic;
 
+CREATE TABLE animals (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ name VARCHAR(100),
+ date_of_birth DATE,
+ escape_attempts INT,
+ neutered BOOLEAN DEFAULT false,
+ weight_kg DECIMAL,
+ PRIMARY KEY(id)
+ );
+
+
 
